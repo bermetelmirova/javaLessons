ee0f4f8b8dfb41170bc3e98006c73ee78a357215
Bermet <bermetk.6789@gmail.com>
2021-06-19 20:21:49 +0600

    not oop style

--- javaLesson/src/com/company/Main.java
+++ javaLesson/src/com/company/Main.java
@@ -1,11 +1,38 @@
 package com.company;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static Random random = new Random();
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        ArrayList <String > input = new ArrayList<>();
+        input.addAll( reader("input.txt"));
+        ArrayList <String > pattern = new ArrayList<>();
+        pattern.addAll( reader("pattern.txt"));
+        ArrayList <String > result = new ArrayList<>();
+        ArrayList <String > result2 = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < pattern.size(); j++) {
+                if(input.get(i).contains(pattern.get(j))){
+                    System.out.println(input.get(i));
+                }
+            }
+
+        }
+    }
+    static List<String> reader(String nameFile)throws IOException{
+        FileReader fileReader = new FileReader(nameFile);
+        Scanner scanner = new Scanner(fileReader);
+        ArrayList <String > input = new ArrayList<>();
+        while(scanner.hasNextLine()){
+            input.add(scanner.nextLine());
+        }
+        fileReader.close();
+        return input;
     }
 }
